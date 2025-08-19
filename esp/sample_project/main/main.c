caec8dc6b699699ba46503cd9bea0845adc83db2
tamnm22 <minhtam221220@gmail.com>
2025-08-19 11:07:37 +0900

    example uart

--- esp/sample_project/main/main.c
+++ esp/sample_project/main/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
@@ -6,13 +7,21 @@
 
 #include "tgpio.h"
 #include "exti.h"
+#include "tuart.h"
 
 #define LED 2
 
 void input_event_callback(int pin){
     if(pin == GPIO_NUM_0){
         gpio_toggle(LED);
+
+        //uart_put((uint8_t*)"Hello Tam.\n", strlen("Hello Tam.\n"));
     }
+}
+
+void tam_uart_callback(uint8_t *data, uint16_t length){
+    //printf("%s", data);
+    uart_put(data, length);
 }
 
 void app_main(void)
@@ -20,6 +29,9 @@ void app_main(void)
     pinMode(LED, GPIO_MODE_INPUT_OUTPUT);
     EXTI_init(GPIO_NUM_0, GPIO_MODE_INPUT, GPIO_INTR_NEGEDGE);
     input_set_callback(input_event_callback);
+
+    uart_init();
+    uart_set_callback(tam_uart_callback);
 
     // while(1){
     //     gpio_toggle(LED);
